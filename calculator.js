9e91d2259fe08f1c1a793a41f75f14eed377162b
Lon <lonbouweriks@gmail.com>
2020-03-08 20:00:28 +0100

    added a  bmi calculator to it

--- calculator.js
+++ calculator.js
@@ -10,7 +10,16 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html")
 });
 
+app.get('/bmiCalculator', function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+});
 
+app.post('/bmiCalculator', function(req, res){
+  var height=Number(req.body.height);
+  var weight=Number(req.body.weight);
+  var result=(weight/(((height/100)*(height/100))));
+  res.send("Your bmi is " + Math.floor(result))
+})
 
 
 app.post("/", function(req, res){
